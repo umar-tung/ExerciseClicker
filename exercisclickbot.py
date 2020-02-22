13efeb05da5004be76f3c6ab2eaea653139e494b
Umar Tung <umartung@gmail.com>
2020-02-22 01:18:44 -0800

    create working bot

--- exercisclickbot.py
+++ exercisclickbot.py
@@ -1,16 +1,16 @@
 from selenium import webdriver
 import time
 
-options = webdriver.ChromeOptions()
-options.add_argument('--ignore-certificate-errors')
-options.add_argument("--test-type")
-options.add_argument("--incognito")
-options.binary_location = "/usr/bin/chromium"
-driver = webdriver.Chrome(chrome_options=options)
-driver.get('https://pbchealthchallenge.ca/resolutions/5e4ebfd1c075624665/')
+while(True):
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument("--test-type")
+    options.add_argument("--incognito")
+    options.binary_location = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+    driver = webdriver.Chrome(executable_path="./chromedriver.exe",options=options)
+    driver.get('https://pbchealthchallenge.ca/resolutions/5e4ebfd1c075624665/')
 
-# click submit button
-submit_button = driver.find_elements_by_xpath('//*[@id="editor"]/table/tbody/tr[3]/td/table/tbody/tr/td/div/table/tbody/tr/td[3]/input')[0]
-submit_button.click()
-
-
+    # click submit button
+    submit_button = driver.find_element_by_class_name('button')
+    submit_button.click()
+    driver.close()
